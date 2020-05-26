cca62e7bac398ee47a9cb42fb2273e570b7b54eb
vitalie_jurjiu_creo <vjurjiu@creo.ca>
2020-05-26 18:28:59 -0400

    Save

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@ ssh -t -t ubuntu@localhost <<EOF
     cd ~/public_html/nodejs/vitalie.ml
     git pull
     npm install --production
-    pm2 restart vitalie.ml
+    pm2 restart vitalie.ml --update-env
     exit
 EOF
