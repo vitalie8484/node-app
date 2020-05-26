00d091dfe528b654a0a5fb6d43e73b5df86b2c30
vitalie_jurjiu_creo <vjurjiu@creo.ca>
2020-05-26 16:31:39 -0400

    save

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,6 @@
 ssh ubuntu@localhost <<EOF
     cd ~/public_html/nodejs/vitalie.ml
     git pull
-    export PATH=/home/ubuntu/.nvm/versions/node/v10.20.1/bin/npm
-    npm install
+    pm2 restart all
     exit
 EOF
